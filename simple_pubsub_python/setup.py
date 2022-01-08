4557bc5536240cfab070d9ad048e3c64cf771162
plantstoen <plantstoen@gmail.com>
2022-01-09 02:22:52 +0900

    init

---
+++ simple_pubsub_python/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'simple_pubsub_python'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='plantstoen',
+    maintainer_email='plantstoen@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'basic_publisher = simple_pubsub_python.basic_publisher:main',
+            'basic_subsriber = simple_pubsub_python.basic_subscriber:main'
+        ],
+    },
+)

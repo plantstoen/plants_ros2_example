43f4a9361118ae20e99f57ba7a413712ae6bc548
plantstoen <plantstoen@gmail.com>
2022-01-13 09:42:05 +0900

    add: param & launch example

--- simple_action_python/simple_action_python/fibonacci_action_client.py
+++ simple_action_python/simple_action_python/fibonacci_action_client.py
@@ -11,30 +11,34 @@ class FibonacciActionClient(Node):
         super().__init__('fibonacci_action_client')
         self._action_client = ActionClient(self, Fibonacci, 'fibonacci')
 
+    # 1번
     def send_goal(self, order):
-        goal_msg = Fibonacci.Goal()
-        goal_msg.order = order
+        goal_msg = Fibonacci.Goal() # action파일의 goal 생성
+        goal_msg.order = order # request시 사용할 데이터 지정 
 
-        self._action_client.wait_for_server()
-        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.feedback_callback)
-        self._send_goal_future.add_done_callback(self.goal_response_callback)
+        self._action_client.wait_for_server() # action server가 응답할때까지 대기
+        self._send_goal_future = self._action_client.send_goal_async(goal_msg, feedback_callback=self.c_feedback_callback)
+        self._send_goal_future.add_done_callback(self.c_goal_response_callback)
 
-    def goal_response_callback(self, future):
+    # 2번
+    def c_goal_response_callback(self, future):
         goal_handle = future.result() # Goal Service에서의 response
         if not goal_handle.accepted:
             self.get_logger().info('Goal rejected :(')
             return
 
         self.get_logger().info('Goal accepted :)')
-        self._get_result_future = goal_handle.get_result_async()
-        self._get_result_future.add_done_callback(self.get_result_callback)
+        self._get_result_future = goal_handle.get_result_async() # 3번
+        self._get_result_future.add_done_callback(self.c_get_result_callback)
 
-    def get_result_callback(self, future):
+    # 5번
+    def c_get_result_callback(self, future):
         result = future.result().result
         self.get_logger().info('Result: {0}'.format(result.sequence))
         rclpy.shutdown()
 
-    def feedback_callback(self, feedback_msg):
+    # 4번
+    def c_feedback_callback(self, feedback_msg):
         feedback = feedback_msg.feedback
         self.get_logger().info('Received feedback: {0}'.format(feedback.partial_sequence))
 
